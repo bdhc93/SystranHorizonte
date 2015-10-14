2060abdab1ee34721c196879d806827acf229b8f
bdhc93 <bdhc93@gmail.com>
2015-10-13 19:58:52 -0500

    pruebas

---
+++ SystranHorizonteWeb.Tests/Controllers/CargaControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystranHorizonteWeb;
+using SystranHorizonte.Models;
+using SystranHorizonte.Services.Ventas.Interfaces;
+using SystranHorizonteWeb.Controllers;
+
+namespace SystranHorizonteWeb.Tests.Controllers
+{
+    [TestClass]
+    public class CargaControllerTest
+    {
+        public ICargaService cargaService { get; set; }
+        
+        [TestMethod]
+        public void CargaControllerGuardar()
+        {
+            CargasController controller = new CargasController(cargaService);
+
+            Carga model = new Carga
+            {
+                Estado = true,
+                Peso = "0 kg Prueba",
+                Precio = 10,
+                Tipo = true,
+                TipoString = "Encomiendas"
+            };
+
+            ViewResult result = controller.AddCarga(model) as ViewResult;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CargaControllerModificar()
+        {
+            CargasController controller = new CargasController(cargaService);
+
+            Int32 idPrueba = 0;
+
+            ViewResult result = controller.Modificar(idPrueba) as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CargaControllerEliminar()
+        {
+            CargasController controller = new CargasController(cargaService);
+
+            Int32 idPrueba = 0;
+
+            ViewResult result = controller.Eliminar(idPrueba) as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ SystranHorizonteWeb.Tests/Controllers/ClienteControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystranHorizonte.Repository.Ventas.Datos;
+using SystranHorizonte.Models;
+
+namespace SystranHorizonteWeb.Tests.Controllers
+{
+    [TestClass]
+    public class ClienteControllerTest
+    {
+        public ClienteRepository clienteRepository { get; set; }
+        
+        [TestMethod]
+        public void ClienteRepGuardar()
+        {
+            Cliente cliente = new Cliente {
+                DniRuc = "73136701",
+                Nombre = "Billy Davis",
+                Apellidos = "Prueba",
+                Direccion = "Direccion Prueba",
+                Telefono = "9797997"
+            };
+
+            clienteRepository.GuardarCliente(cliente);
+        }
+        
+    }
+}
